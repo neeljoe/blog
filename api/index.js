3150b64c57eed7e2a5dda8b47000529c5cc107aa
Shelton Joseph <joe.neel@gmail.com>
2025-04-03 22:40:39 +0530

    routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose, { Mongoose } from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
+import userRouter from './routes/user.route.js';
 
 mongoose.connect(process.env.MONGO).then( () => {
     console.log("MongoDB is connected");
@@ -16,3 +17,4 @@ app.listen(3000, () => {
     console.log("server is listening on port 3000 !");
     
 })
+app.use("/api/user", userRouter);
---
+++ api/routes/user.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get("/test", (req, res) => {
+    res.json({message:"Api is working"});
+});
+
+export default router;
