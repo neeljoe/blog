547603848f1d07fff923bf9e1188f3c6a3fc4427
neeljoe <joe.neel@gmail.com>
2025-04-04 16:27:05 +0530

    hashed

--- api/index.js
+++ api/index.js
@@ -3,18 +3,21 @@ import mongoose, { Mongoose } from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'
 
 mongoose.connect(process.env.MONGO).then( () => {
-    console.log("MongoDB is connected");
-    
+    console.log("MongoDB is connected");  
 }).catch(err => {
-    console.log(err);
-    
+    console.log(err);   
 })
-
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/user", userRouter);
+app.use('/api/auth', authRouter);
+
 app.listen(3000, () => {
     console.log("server is listening on port 3000 !");
     
 })
-app.use("/api/user", userRouter);
