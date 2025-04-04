bb889810c49729c3f47e6c255ed439b33c20057a
Shelton Joseph <joe.neel@gmail.com>
2025-04-04 23:16:29 +0530

    hero

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,9 +4,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <Button>
-      Home
-      </Button>
+     Home
     </div>
   )
 }
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-5 md:p-8 lg:p-10 mx-auto'>
+        {/* left side */}
+        <div className='md:w-1/2 flex flex-col items-center justify-center '>
+          <p className='text-5xl my-logo text-rose-600 font-bold md:text-6xl lg:text-7xl mb-8'>Your Life, <span>Your Style</span></p>
+          <p className='text-lg'>Discover your unique personal style, find inspiration for a vibrant lifestyle, well-being, and start charting your path towards your goals. Let's make these years your most confident and authentic yet!</p>
+
+        </div>
+        {/* right side */}
+        <div className='md:w-1/2'>
+
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
